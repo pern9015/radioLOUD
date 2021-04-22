714831320f0d32d305a7c39b7e1aa8abd6220425
Julie Thorenfeldt <juliethorenfeldt@MacBook-Pro-tilhrende-Julie.local>
2021-04-22 14:16:38 +0200

    single podcast

--- wordpress/wp-content/themes/childtheme/single-podcast.php
+++ wordpress/wp-content/themes/childtheme/single-podcast.php
@@ -2,41 +2,103 @@
 get_header(); ?>
 
 
+
+
 <!--Content-->
 
-<main id="main" class="site-main">
+<section id="primary" class="content-area">
+    <main id="main" class="site-main">
+        <h1>Podcasten</h1>
+        <article>
+            <img src="" alt="">
+            <div>
+                <h1></h1>
+                <p class="beskrivelse"></p>
+            </div>
+        </article>
 
-    <section class="first_section">
-        <img src="assets/images/Asset_13.svg" alt="lyd">
-    </section>
-    <article class="radio" id="podcasts">
-        <img src="" alt="" class="billede_single">
-        <h3 class="title"></h3>
-        <p class="lang_beskrivelse"></p>
-    </article>
-</main>
+        <section id="episoder">
+            <template>
+                <article>
+                    <img src="" alt="">
+                    <div>
+                        <h2></h2>
+                        <p class="beskrivelse"></p>
+                        <a href="">Læs mere</a>
+                    </div>
+                </article>
+            </template>
+        </section>
+    </main>
 
-<script>
-    let podcast;
 
-    const dbUrl = "http://pernillestrate.dk/radioLOUD/wordpress/wp-json/wp/v2/podcast/" + <?php echo get_the_ID() ?>;
+    <script>
+        let podcast;
+        let episoder;
+        let aktuelpodcast = <?php echo get_the_ID() ?>;
 
-    async function getJson() {
-        const data = await fetch(dbUrl);
-        podcast = await data.json();
-        console.log(podcast);
-        visPodcasts();
-    }
+        const dbUrl = "http://pernillestrate.dk/radioLOUD/wordpress/wp-json/wp/v2/podcast/" + podcast;
+        const episodeUrl = "http://pernillestrate.dk/radioLOUD/wordpress/wp-json/wp/v2/episoder?per_page=100"
 
-    function visPodcasts() {
-        console.log(podcast.billede.guid);
-        //document.querySelector("h3").textContent = drink.title.rendered;
-        document.querySelector(".billede_single").src = podcast.billede.guid;
-        document.querySelector(".lang_beskrivelse").textContent = podcast.lang_beskrivelse;
-        document.querySelector(".title").textContent = podcast.title.rendered;
-    }
+        const container = document.querySelector("#episoder");
+
+        async function getJson() {
+            const data = await fetch(dbUrl);
+            podcast = await data.json();
+
+            const data2 = await fetch(episodeUrl);
+            episoder = await data2.json();
+            console.log("episoder: ", episode);
+
+            visPodcasts();
+            visEpisoder();
+        }
+
+
+        function visPodcasts() {
+            console.log("visPodcasts");
+            console.log(podcast.title.rendered);
+            document.querySelector("h1").innerHTML = podcast.title.rendered;
+            document.querySelector(".beskrivelse").innerHTML = podcast.content.rendered;;
+        }
+
+        function visEpisoder() {
+            console.log("visEpisoder");
+            let temp = document.querySelector("template");
+            episoder.forEach(episode => {
+                console.log("loop id :", aktuelpodcast);
+                if (episode.horer_til_podcast == aktuelpodcast) {
+
+                    console.log("loop kører id :", aktuelpodcast);
+                    let klon = temp.cloneNode(true).content;
+                    klon.querySelector("h2").textContent = episode.title.rendered;
+
+                    klon.querySelector(".beskrivelse").innerHTML = episode.content.rendered;
+                    klon.querySelector("article").addEventListener("click", () => {
+                        location.href = episode.link;
+                    })
+
+                    klon.querySelector("a").href = episode.link;
+                    console.log("episode", episode.link);
+                    container.appendChild(klon);
+                }
+
+
+            })
+
+        }
+
+    </script>
+
+
+</section>
+
+
+
+
+
 
     getJson();
 
-</script>
+
 <?php get_footer(); ?>
