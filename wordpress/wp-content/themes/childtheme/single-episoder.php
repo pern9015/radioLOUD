eb87dde35921351f189f2f3f82710037b261fb35
Pernille Strate <pernillestrate@Pernilles-MacBook-Pro.local>
2021-04-29 09:49:56 +0200

    streaming - single episoder

--- wordpress/wp-content/themes/childtheme/single-episoder.php
+++ wordpress/wp-content/themes/childtheme/single-episoder.php
@@ -3,42 +3,37 @@
 get_header(); ?>
 
 <main id="main" class="site-main">
-    <section id="first_section_episode">
-        <br>
-        <!--        <img src=" assets/images/Asset_13.svg" alt="lyd">-->
-    </section>
+
     <article class="radio" id="podcasts">
         <img src="" alt="" class="lydbolge">
     </article>
 
-    <div class="baggrund">
-        <section id="episoder">
-            <template>
-                <article class="article_episode">
-                    <img src="" alt="" class="billede_episode">
-                    <div>
-                        <h3 class="episode_title"></h3>
-                        <p class="beskrivelse"></p>
-                        <a href="">Læs mere</a>
-                    </div>
-                </article>
-            </template>
+    <div class="baggrund_episode">
+        <section id="episoder_grid">
+
+            <article class="article_episode">
+                <img src="" alt="billede mangler" class="billede_episode">
+                <div>
+                    <h3 class="episode_title"></h3>
+                    <p class="beskrivelse"></p>
+                    <a href="">Læs mere</a>
+                </div>
+            </article>
+
         </section>
     </div>
-    <section id="third_section">
-        <br>
-    </section>
+
 </main>
 
 
 <script>
-    let podcast;
-    let episoder;
-    let aktuelpodcast = <?php echo get_the_ID() ?>;
+    let episode;
+    //    let episoder;
+    let aktuelepisode = <?php echo get_the_ID() ?>;
 
 
-    const dbUrl = "http://pernillestrate.dk/radioLOUD/wordpress/wp-json/wp/v2/episoder/" + aktuelpodcast;
-    const episodeUrl = "http://pernillestrate.dk/radioLOUD/wordpress/wp-json/wp/v2/episoder?per_page=100";
+    const dbUrl = "http://pernillestrate.dk/radioLOUD/wordpress/wp-json/wp/v2/episoder/" + aktuelepisode;
+    //    const episodeUrl = "http://pernillestrate.dk/radioLOUD/wordpress/wp-json/wp/v2/episoder?per_page=100";
 
     const container = document.querySelector("#episoder");
 
@@ -46,41 +41,48 @@ get_header(); ?>
 
     async function getJson() {
         const data = await fetch(dbUrl);
-        podcast = await data.json();
-        console.log(podcast);
+        episode = await data.json();
+        console.log("episode: ", episode);
 
-        const data2 = await fetch(episodeUrl);
-        episoder = await data2.json();
-        console.log("episoder: ", episoder);
+        //        const data2 = await fetch(episodeUrl);
+        //        episoder = await data2.json();
+        //        console.log("episoder: ", episoder);
 
-        visEpisoder();
+        visEpisode();
     }
 
 
-    function visEpisoder() {
-        console.log("visEpisoder");
-        let temp = document.querySelector("template");
-        episoder.forEach(episode => {
-            console.log("loop id :", aktuelpodcast);
-            if (episode.horer_til_podcast == aktuelpodcast) {
+    function visEpisode() {
+        console.log("visEpisode");
+        //        let temp = document.querySelector("template");
+        //        episoder.forEach(episode => {
+        //            console.log("loop id :", aktuelepisode);
+        //            console.log("episode", episode.horer_til_podcast);
+        //            if (episode.horer_til_podcast == aktuelepisode) {
+        //
+        //                console.log("loop kører id :", aktuelepisode);
+        //                let klon = temp.cloneNode(true).content;
+        //                klon.querySelector("h3").textContent = episode.title.rendered;
+        //                klon.querySelector("img").src = episode.billede.guid;
+        //                console.log("episode.title.rendered: ", episode.title.rendered);
+        //                klon.querySelector(".beskrivelse").innerHTML = episode.content.rendered;
+        //                klon.querySelector("article").addEventListener("click", () => {
+        //                    location.href = episode.link;
+        //                })
+        //
+        //                klon.querySelector("a").href = episode.link;
+        //                console.log("episode", episode.link);
+        //                container.appendChild(klon);
+        //            }
 
-                console.log("loop kører id :", aktuelpodcast);
-                let klon = temp.cloneNode(true).content;
-                klon.querySelector("h3").textContent = episode.title.rendered;
-                klon.querySelector("img").src = episode.billede.guid;
-                console.log("episode.title.rendered: ", episode.title.rendered);
-                klon.querySelector(".beskrivelse").innerHTML = episode.content.rendered;
-                klon.querySelector("article").addEventListener("click", () => {
-                    location.href = episode.link;
-                })
 
-                klon.querySelector("a").href = episode.link;
-                console.log("episode", episode.link);
-                container.appendChild(klon);
-            }
+        //        })
 
 
-        })
+        document.querySelector(".episode_title").textContent = episode.title.rendered
+        document.querySelector(".beskrivelse").innerHTML = episode.content.rendered;
+        document.querySelector(".billede_episode").alt = episode.title.rendered
+        document.querySelector(".billede_episode").src = episode.billede.guid;
 
     }
     getJson();
